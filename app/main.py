1e8e1858f86b70beb271d1849379412e1cfd1076
ivanzakharov <ivanzakharov0707@gmail.com>
2025-09-11 18:40:56 +0300

    Add database connection and Word model

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
+from . import models
+from .database import engine
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title = "Japanese Word Repetition API", version = "0.1")
 
 @app.get("/")
 def root():
-    return {"message": "Japanese Word Repetition API работает!"}
-
-@app.get("/hello")
-def say_hello(name: str = "User"):
-    return {"message": f"Привет, {name}!"}
+    return {"message": "Japanese Word Repetition API работает и подключено к БД!"}
---
+++ app/models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from .database import Base
+import datetime
+
+class Word(Base):
+    __tablename__ = "words"
+
+    id = Column(Integer, primary_key=True, index=True)
+    japanese = Column(String,index=True)
+    translation = Column(String)
+    example = Column(String, nullable=True)
+    level = Column(Integer, default = 0)
+    next_review = Column(DateTime, default=datetime.datetime.utcnow)
